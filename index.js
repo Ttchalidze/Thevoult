caeb17a5ac9f6d74111b08cc2b5e2d17c7364144
tornike thcalidze <ttchalidze@gmail.com>
2025-02-09 22:34:56 -0600

    JS2

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ let code1;
 code1 = 7 + 3;
 console.log(code1);
 let code2;
-code2 = 90 / 2 - 5;
+code2 = (120 - 40) / 2;
 console.log(code2);
 let code3;
-code3 = 80 / 2 - 1;
+code3 = 78 % 40;
 console.log(code3);
 let message;
 message = "The vault has been secured. The combination is:";
 console.log(message);
 let codeA;
-codeA = code1 - code2 - code3;
+codeA = code1 + "-" + code2 + "-" + code3;
 console.log(codeA);
-let code8;
-code8 = code1 - code2 - code3;
-console.log(
-  `code8 is ${code1 - code2 - code3} and not ${code1 + code2 + code3}.`
-);
+let codeb;
+codeb = `${code1}-${code2}-${code3}`;
+console.log(codeb);
